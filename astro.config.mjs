034c167b6c31aba3c8ffea7f51006c72359b421f
Roudranil <nil0auditore78@gmail.com>
2023-12-16 10:19:42 +0530

    updated astro config with integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import remarkCodeTitles from "remark-code-titles";
 
 import tailwind from "@astrojs/tailwind";
+import catppuccinMocha from "./catppuccin-mocha.json";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    markdown: {
+        remarkPlugins: [remarkMath, remarkCodeTitles],
+        rehypePlugins: [rehypeKatex],
+        shikiConfig: {
+            theme: catppuccinMocha,
+        },
+    },
+    integrations: [tailwind(), mdx()],
 });
